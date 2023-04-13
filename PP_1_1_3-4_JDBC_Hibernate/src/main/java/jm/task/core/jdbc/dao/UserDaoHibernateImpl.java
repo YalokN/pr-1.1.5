5203c8644579e052b6dfe29126d56076743dbc61
YalokN <118694186+YalokN@users.noreply.github.com>
2023-04-13 15:13:45 +0500

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,107 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.Query;
+
+import static jm.task.core.jdbc.util.Util.getSessionFactory;
+
+public class UserDaoHibernateImpl implements UserDao {
+    public void saveStudent(User student) {
+        Transaction transaction = null;
+        try (Session session = getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the student object
+            session.save(student);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    public UserDaoHibernateImpl() {
+
+    }
+
+
+    @Override
+    public void createUsersTable() {
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            String sql = "CREATE TABLE IF NOT EXISTS User " +
+                    "(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(120) NOT NULL, lastName VARCHAR(120) NOT NULL, " +
+                    "age TINYINT(1) NOT NULL)";
+
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            query.executeUpdate();
+
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void dropUsersTable() {
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            String sql = "DROP TABLE IF EXISTS User";
+
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            query.executeUpdate();
+
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        try (Session session = getSessionFactory().openSession()) {
+            return session.createQuery("FROM User", User.class).list();
+        }
+    }
+
+    @Override
+    public void cleanUsersTable() {
+        try (Session session = getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            String sql = "TRUNCATE User";
+
+            Query query = session.createSQLQuery(sql).addEntity(User.class);
+            query.executeUpdate();
+
+            transaction.commit();
+        }
+    }
+}
